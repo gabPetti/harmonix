55a2c1abf2d85e98ce12477094a8a589ea7b8a4e
gabpetti <gabpettidev@gmail.com>
2026-09-01 17:26:40 +0100

    feat: add utilities to zsh

--- modules/core/zsh.nix
+++ modules/core/zsh.nix
@@ -6,19 +6,27 @@
     enableCompletion = true;
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
-
     promptInit = ''
       eval "$(${pkgs.starship}/bin/starship init zsh)"
     '';
     shellAliases = {
-      ll = "ls -l";
+      cd = "z";
+      ls = "eza --icons --group-directories-first";
     };
-
     histSize = 10000;
     histFile = "$HOME/.zsh_history";
     setOptions = [
       "HIST_IGNORE_ALL_DUPS"
     ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    eza
+  ];
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
   };
 
   users.users.gabpetti.shell = pkgs.zsh;
