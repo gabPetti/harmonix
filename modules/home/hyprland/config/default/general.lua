8da2e18521ea6f48b04249974adaf2e0d8920fca
gabpetti <gabpettidev@gmail.com>
2026-09-10 15:11:46 +0100

    feat: change layout on hyprland

--- modules/home/hyprland/config/default/general.lua
+++ modules/home/hyprland/config/default/general.lua
@@ -1,38 +1,30 @@
 hl.config({
-  -- ##==> Inputs
-  input = {
-    kb_layout = "br",
-    kb_options = "ctrl:swapcaps,altwin:swap_lalt_lwin",
-    touchpad = {
-      natural_scroll = true,
-    },
-  },
+	-- ##==> Inputs
+	input = {
+		kb_layout = "br",
+		kb_options = "ctrl:swapcaps,altwin:swap_lalt_lwin",
+		touchpad = {
+			natural_scroll = true,
+		},
+	},
 
-  -- ##==> Layouts
-  dwindle = {
-    preserve_split = true
-  },
+	-- ##==> Layouts
+	general = {
+		layout = "master",
+	},
 
-  master = {
-    new_status = "master",
-  },
+	-- ##==> Misc
+	misc = {
+		vrr = 0,
+		disable_hyprland_logo = true,
+		disable_splash_rendering = true,
+		force_default_wallpaper = 0,
+		anr_missed_pings = 5,
+		allow_session_lock_restore = true,
+	},
 
-  general = {
-    layout = "dwindle",
-  },
-
-  -- ##==> Misc
-  misc = {
-    vrr = 0,
-    disable_hyprland_logo = true,
-    disable_splash_rendering = true,
-    force_default_wallpaper = 0,
-    anr_missed_pings = 5,
-    allow_session_lock_restore = true,
-  },
-
-  -- ##==> XWayland
-  xwayland = {
-    force_zero_scaling = true,
-  },
+	-- ##==> XWayland
+	xwayland = {
+		force_zero_scaling = true,
+	},
 })
