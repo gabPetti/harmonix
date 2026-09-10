aeaa8676af22e6a25bad1bf5032f295a43b9a5a6
gabpetti <gabpettidev@gmail.com>
2026-09-10 13:28:36 +0100

    feat: remove vivaldi

--- modules/home/vivaldi/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, config, ... }:
-
-{
-  home.packages = with pkgs; [
-    vivaldi
-  ];
-
-  xdg.configFile."vivaldi/Default/Preferences".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/harmonix/modules/home/vivaldi/config/Preferences";
-}
--- users/gabpetti/home.nix
+++ users/gabpetti/home.nix
@@ -14,7 +14,6 @@
     ../../modules/home/yazi
     ../../modules/home/rofi
     ../../modules/home/matugen
-    ../../modules/home/vivaldi
   ];
 
   services.playerctld.enable = true;
