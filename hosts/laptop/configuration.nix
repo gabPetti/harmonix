6505bbd9fb8272d9c25c02427ae322a4807c28a2
gabpetti <gabpettidev@gmail.com>
2026-09-11 19:34:11 +0100

    feat: add nvim as default file viewer

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -12,6 +12,12 @@
   ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
+
   nixpkgs.config.allowUnfree = true;
 
   virtualisation.docker.enable = true;
--- modules/core/xdg.nix
+++ modules/core/xdg.nix
@@ -1,6 +1,16 @@
 { pkgs, ... }:
 
 {
+  environment.systemPackages = [
+    (pkgs.makeDesktopItem {
+      name = "nvim-term";
+      desktopName = "Neovim (Terminal)";
+      exec = "${pkgs.kitty}/bin/kitty -e ${pkgs.neovim}/bin/nvim %F";
+      icon = "nvim";
+      mimeTypes = [ "text/plain" "text/markdown" "application/json" "text/x-shellscript" ];
+    })
+  ];
+
   xdg = {
     # 1. Enable and configure Desktop Portals
     portal = {
@@ -15,12 +25,12 @@
         };
       };
     };
-  };
 
-  # dconf.settings = {
-  #   "org/gnome/desktop/interface" = {
-  #     color-scheme = "prefer-dark";
-  #     font-name = "JetBrainsMono Nerd Font 10";
-  #   };
-  # };
+    mime.defaultApplications = {
+      "text/plain" = "nvim-term.desktop";
+      "text/markdown" = "nvim-term.desktop";
+      "application/json" = "nvim-term.desktop";
+      "text/x-shellscript" = "nvim-term.desktop";
+    };
+  };
 }
